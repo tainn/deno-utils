dcb401156d0065853d23887fcffec97f146bdfde
tainn <tainn@protonmail.com>
2026-04-17 12:31:19 +0200

    loosen up the rii function

--- src/random.ts
+++ src/random.ts
@@ -9,6 +9,14 @@ import { randomInt } from "node:crypto";
  * const dice = rii(1, 6);
  * ```
  */
-export function rii(min: number, max: number): number {
+export function rii(min: number, max: number): number | null {
+  if (Number.isNaN(min) || Number.isNaN(max)) {
+    return null;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
   return randomInt(min, max + 1);
 }
